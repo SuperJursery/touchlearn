febbbb4191c596771fee4a0b2be39ee668160780
liufl <liufl@nowada.com>
2021-04-24 17:06:13 +0800

    Add one button switch and modify layout

--- app/src/main/java/com/jursery/app/touchlearn/TouchconfigMenu.java
+++ app/src/main/java/com/jursery/app/touchlearn/TouchconfigMenu.java
@@ -1,12 +1,26 @@
 package com.jursery.app.touchlearn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class TouchconfigMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button btn_cab = (Button) this.findViewById(R.id.btn_cab);
+        btn_cab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                //显式方式 切换activity
+                Intent intent = new Intent();
+                intent.setClass(TouchconfigMenu.this, calibration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
